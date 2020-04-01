92e82062e79e5da95e5775337220df7f9dba960d
enrico25 <enrico.reitberger@googlemail.com>
2020-04-01 15:39:56 +0200

    template

---
+++ src/test/java/com/inventoryapp/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventoryapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
