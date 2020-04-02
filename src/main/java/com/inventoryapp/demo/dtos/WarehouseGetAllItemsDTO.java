470d423e10c6d14c93303a377b07d5dae68948d1
Monica Hernandez Ascencio <monicaherasc@gmail.com>
2020-04-02 12:09:48 +0200

    commit to be able to merge

---
+++ src/main/java/com/inventoryapp/demo/dtos/WarehouseGetAllItemsDTO.java
@@ -0,0 +1,16 @@
+package com.inventoryapp.demo.dtos;
+
+import lombok.Data;
+
+@Data
+public class WarehouseGetAllItemsDTO {
+    private String category;
+    private int quantitiy;
+    private long pricePerUnit;
+
+    public WarehouseGetAllItemsDTO (String category, int quantitiy, long pricePerUnit){
+        this.category = category;
+        this.quantitiy = quantitiy;
+        this.pricePerUnit = pricePerUnit;
+    }
+}
