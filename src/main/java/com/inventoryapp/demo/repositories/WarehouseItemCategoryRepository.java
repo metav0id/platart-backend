41ded4e1586f64a29bc5e4b77220fc3499830ef0
Alan Schmollgruber <alanschmollgruber@gmail.com>
2020-04-15 12:54:54 +0200

    New Functionality for deleting categories.

--- src/main/java/com/inventoryapp/demo/repositories/WarehouseItemCategoryRepository.java
+++ src/main/java/com/inventoryapp/demo/repositories/WarehouseItemCategoryRepository.java
@@ -2,8 +2,18 @@ package com.inventoryapp.demo.repositories;
 
 import com.inventoryapp.demo.entities.WarehouseItemCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+
 public interface WarehouseItemCategoryRepository extends JpaRepository<WarehouseItemCategory, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query("delete from WarehouseItemCategory item where item.category=:category")
+    void deleteCategoryByName(@Param("category") String category);
 }
