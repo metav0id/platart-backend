43dfdf5e2d251abb1bf1ffc7b5c9075580643520
Monica Hernandez Ascencio <monicaherasc@gmail.com>
2020-05-05 11:13:56 +0200

    get all stores method implemented under: http://localhost:8081/comerce/getallshops

--- src/main/java/com/inventoryapp/demo/repositories/ComerceRepository.java
+++ src/main/java/com/inventoryapp/demo/repositories/ComerceRepository.java
@@ -2,7 +2,13 @@ package com.inventoryapp.demo.repositories;
 
 import com.inventoryapp.demo.entities.Comerce;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+import java.util.List;
 
 public interface ComerceRepository extends JpaRepository<Comerce, Long> {
 
+    @Query(value = "SELECT entry FROM Comerce entry WHERE entry.category = 'store'")
+    List<Comerce> findByCategory();
 }
