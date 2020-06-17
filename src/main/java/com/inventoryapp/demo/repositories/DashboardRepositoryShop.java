aed97eef4fbdc603056a22958b8301fb1a843a95
metav0id <volkerhauck@def-design.de>
2020-06-17 14:31:35 +0200

    Backend Dashboard finished

--- src/main/java/com/inventoryapp/demo/repositories/DashboardRepositoryShop.java
+++ src/main/java/com/inventoryapp/demo/repositories/DashboardRepositoryShop.java
@@ -3,16 +3,17 @@ package com.inventoryapp.demo.repositories;
 import com.inventoryapp.demo.entities.ShopsAllSoldItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface DashboardRepositoryShop extends JpaRepository<ShopsAllSoldItems,Long> {
 
     @Query("SELECT i FROM ShopsAllSoldItems i WHERE i.itemLastSold <= :end AND i.itemLastSold >= :start")
-    List<ShopsAllSoldItems> findByItemLastSoldDateMin(java.sql.Date start, java.sql.Date end);
-
-
-
+    List<ShopsAllSoldItems> findByItemLastSoldDateMin(@Param("start")LocalDateTime start, @Param("end")LocalDateTime end);
 
 }
 
